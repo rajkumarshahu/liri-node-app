8f02266ab8b117abb343f00f28e71f0c5d2269a9
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-28 19:11:04 -0400

    getVenue function created

--- liri.js
+++ liri.js
@@ -8,18 +8,18 @@ var moment = require('moment');
 moment().format();
 
 var command = process.argv[2];
-var song = process.argv[3];
+var userSearchKey = process.argv[3].trim();
 
 function spotifyThis(){
     if(command == "spotify-this-song"){
         spotify.search({
             type: "track",
-            query: song
+            query: userSearchKey
         }, function (err, data) {
             if (err) {
                 console.log("Error occured: " + err);
-                fs.appendFile("log.txt", `, ${err}`, (error)=>{
-                    console.log(error);
+                fs.appendFile("log.txt", `${err}`, (err)=>{
+                    console.log(err);
                 });
             }
             var trackDetail = data.tracks.items
@@ -30,15 +30,15 @@ function spotifyThis(){
             console.log("Preview Link: " + trackDetail[0].preview_url);
             console.log("Album: " + trackDetail[0].album.name);
             console.log(`><><><><><><>Spotified><><><><><><><`);
-            var logData = `\n><><><><><><>Spotifying><><><><><><><\nArtist: ${trackDetail[0].artists[0].name}\nSong Name: ${trackDetail[0].name}\nPreview Link: ${trackDetail[0].preview_url}\nAlbum: trackDetail[0].album.name\n><><><><><><>Spotified><><><><><><><\n`;
+            var logData = `\n><><><><><><>Spotifying><><><><><><><\nArtist: ${trackDetail[0].artists[0].name}\nSong Name: ${trackDetail[0].name}\nPreview Link: ${trackDetail[0].preview_url}\nAlbum: ${trackDetail[0].album.name}\n><><><><><><>Spotified><><><><><><><\n`;
 
-            fs.appendFile("log.txt", `, ${logData}`, function(error){
-                console.log(error);
+            fs.appendFile("log.txt", `${logData}`, function(err){
+                console.log(err);
             });
         });
     }
 }
-spotifyThis();
+//spotifyThis();
 
 
 
@@ -51,18 +51,29 @@ spotifyThis();
 //   }
 // );
 
+function getVenue(){
+    if(command == "concert-this" && userSearchKey != undefined){
+        axios.get("https://rest.bandsintown.com/artists/" + userSearchKey + "/events?app_id=codingbootcamp").then(
+          function(response) {
+                var venueDetail = response.data
+                //console.log(venueDetail[0].venue);
+                console.log("-------------------------------------");
+                console.log("Venue: " + venueDetail[0].venue.name);
+                console.log("Location: " + venueDetail[0].venue.city + " " + venueDetail[0].venue.region);
+                console.log("Date: " + moment(venueDetail[0].datetime).format("MM/DD/YYYY"));
+                console.log("-------------------------------------");
 
+                var logData = `\n><><><><><><>B in T start><><><><><><><\nVenue: ${venueDetail[0].venue.name}\nLocation: ${venueDetail[0].venue.city} ${venueDetail[0].venue.region}\nDate: ${moment(venueDetail[0].datetime).format("MM/DD/YYYY")}\n><><><><><><>B in T end><><><><><><><\n`;
 
+            fs.appendFile("log.txt", `${logData}`, function(err){
+                console.log(err);
+            });
 
-// axios.get("https://rest.bandsintown.com/artists/" + "john mayer" + "/events?app_id=codingbootcamp").then(
-//   function(response) {
-//         var venueDetail = response.data
-//         console.log(venueDetail[0].venue);
-//         console.log("-------------------------------------");
-//         console.log("Venue: " + venueDetail[0].venue.name);
-//         console.log("Location: " + venueDetail[0].venue.city + " " + venueDetail[0].venue.region);
-//         console.log("Date: " + moment(venueDetail[0].datetime).format("MM/DD/YYYY"));
-//         console.log("-------------------------------------");
+          }
+        );
+    }else{
+        console('Venue not found!!!')
+    }
+}
 
-//   }
-// );
+getVenue();
